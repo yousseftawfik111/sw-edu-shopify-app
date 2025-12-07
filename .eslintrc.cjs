ebe6acd6e06e44e1dae8f1931bcd2dadaa8e8351
Youssef Tawfik <youssef.tawfik@scandiweb.com>
2025-12-07 18:09:38 +0200

    Added prisma seed script

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -84,6 +84,7 @@ module.exports = {
         ".graphqlrc.{js,ts}",
         "shopify.server.{js,ts}",
         "**/*.server.{js,ts}",
+        "prisma/**/*.js",
       ],
       env: {
         node: true,
